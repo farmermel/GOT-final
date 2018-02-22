64c30b9cd295470ab76f713383af7ac7df80be5a
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-22 13:31:42 -0700

    Add test for sworn members api call

--- src/helpers/apiCalls.test.js
+++ src/helpers/apiCalls.test.js
@@ -1,35 +1,69 @@
 /* eslint-disable */
-import { firstApiCall } from './apiCalls';
+import { firstApiCall, getSwornMembers } from './apiCalls';
 
-describe('firstApiCall', () => {
-  const mockData = [
-    { Tyrells: 'flowers' },
-    { Lannisters: 'lions' }
-  ]
+describe('apiCalls', () => {
+  describe('firstApiCall', () => {
+    const mockData = [
+      { Tyrells: 'flowers' },
+      { Lannisters: 'lions' }
+    ]
 
-  it('calls fetch with url', () => {
-    window.fetch = jest.fn().mockImplementation(() => {
-      return Promise.resolve({
-        status: 200,
-        json: () => {
-          return mockData
-        }
+    it('calls fetch with url', () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          status: 200,
+          json: () => {
+            return mockData
+          }
+        })
+      })
+      expect(window.fetch).not.toHaveBeenCalled();
+      firstApiCall();
+      expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/api/v1/houses');
+    })
+
+    it('returns an array of fetched houses', async () => {
+      expect(await firstApiCall()).toEqual(mockData);
+    })
+
+    it.skip('throws an error if status is not good', async () => {
+        window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+            status: 500,
+            json: () => {}
+          })
+        )
+        expect(await firstApiCall()).toEqual(Error());
+    })
+  })
+
+  describe('getSwornMembers', () => {
+    const mockData = [
+      {name: 'Lyanna'},
+      {name: 'Nymeria'}
+    ]
+
+    beforeEach(() => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          status: 200,
+          json: () => {
+            return mockData
+          }
+        })
+      })
+      Promise.all = jest.fn().mockImplementation(() => {
+        return mockData
       })
     })
-    expect(window.fetch).not.toHaveBeenCalled();
-    firstApiCall();
-    expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/api/v1/houses');
-  })
 
-  it('returns an array of fetched houses', async () => {
-    expect(await firstApiCall()).toEqual(mockData);
-  })
+    it('calls fetch', async () => {
+      expect(window.fetch).not.toHaveBeenCalled();
+      await getSwornMembers(mockData);
+      expect(window.fetch).toHaveBeenCalled();
+    })
 
-  it('throws an error if status is not good', async () => {
-          window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-          status: 500 
-        })
-      )
-      expect(firstApiCall()).rejects.toEqual(Error());
+    it('returns a string of comma seperated names', async () => {
+      expect(await getSwornMembers(mockData)).toEqual('Lyanna, Nymeria');
+    })
   })
 })
