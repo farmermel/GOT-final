63391a90e6c46d480943691ac3c82787d99c394e
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-22 13:32:03 -0700

    Semicolons for linter

--- src/helpers/apiCalls.js
+++ src/helpers/apiCalls.js
@@ -11,20 +11,17 @@ export const getSwornMembers = async swornMembers => {
   const membersPromises = await swornMembers.map( async member => {
     const initialFetch = await fetch(member, {
       method: 'GET'
-    })
+    });
     return await initialFetch.json();
-  })
-
-  // return await Promise.all(membersPromises);
-
+  });
   const swornMembersResolved = await Promise.all(membersPromises);
   return cleanSwornMembers(swornMembersResolved);
-}
+};
 
 const cleanSwornMembers = swornMembers => {
   const memberNames = swornMembers.map( member => {
     return member.name;
-  } )
+  } );
 
   return memberNames.join(', ');
-}
+};
