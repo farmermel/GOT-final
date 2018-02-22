87edc7a089468a09a9eacf20cc3df35e05f1e6b1
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-02-22 13:31:12 -0700

    Have snapshot for each possible display of card

--- src/components/HouseCard/HouseCard.test.js
+++ src/components/HouseCard/HouseCard.test.js
@@ -6,7 +6,13 @@ import HouseCard from './HouseCard';
 describe('HouseCard', () => {
   const wrapper = shallow(<HouseCard card={{house: 'greyjoy'}}
                                      clicked={false} />)
-  it('matches snapshot', () => {
+  it('matches snapshot when clicked is false', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it('matches snapshot when clicked is true', () => {
+    const wrapper = shallow(<HouseCard card={{house: 'greyjoy'}}
+                                       clicked={true} />)
     expect(wrapper).toMatchSnapshot();
   })
 })
